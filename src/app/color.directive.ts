8eb07a880e2ece05038d9fa04187e9508ef19df3
Jorge Oliveira <Oliveira@Generals-iMac-2.local>
2019-08-21 13:19:51 -0400

    projet4

---
+++ src/app/color.directive.ts
@@ -0,0 +1,45 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  // tslint:disable-next-line: directive-selector
+  selector: '[MyColorDirective]'
+})
+export class ColorDirective {
+  public colorText: string;
+  private element: ElementRef;
+
+  constructor(el: ElementRef) {
+    this.element = el;
+   }
+
+
+
+   @HostListener('window:keydown', ['$event']) windowKeyDown($event) {
+    switch ($event.keyCode) {
+      case 37:
+        this.colorText = 'red';
+        break;
+      case 38:
+        this.colorText = 'blue';
+        break;
+      case 39:
+        this.colorText = 'green';
+        break;
+      case 40:
+        this.colorText = 'orange';
+        break;
+      default:
+        this.colorText = 'black';
+    }
+    this.changeColor();
+   }
+
+   changeColor() {
+    this.element.nativeElement.style.color = this.colorText;
+   }
+
+
+
+
+
+}
